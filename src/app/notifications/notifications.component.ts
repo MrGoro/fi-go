32da58aaa6c5765a5c22fa88e8e3c5dbf5deb447
mrgoro <mail@mrgoro.de>
2018-11-25 12:42:12 +0100

    Notifications UI / service worker

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFireMessaging} from "@angular/fire/messaging";
-import {mergeMap, mergeMapTo} from "rxjs/operators";
+import {mergeMap, mergeMapTo, tap} from "rxjs/operators";
 import {StorageService} from "../shared/storage.service";
+import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-notifications',
@@ -15,6 +16,7 @@ export class NotificationsComponent implements OnInit {
   public supported: boolean = false;
 
   constructor(private afMessaging: AngularFireMessaging,
+              private afStore: AngularFirestore,
               private storage: StorageService) {
   }
 
@@ -23,12 +25,13 @@ export class NotificationsComponent implements OnInit {
     this.supported = true;
 
     this.storage.get('notifications').subscribe(notificationsEnabled => {
-      if(notificationsEnabled || notificationsEnabled == undefined) {
+      if (notificationsEnabled || notificationsEnabled == undefined) {
         console.log('Notifications enabled!');
         this.afMessaging.getToken
           .pipe(mergeMapTo(this.afMessaging.tokenChanges))
           .subscribe(
             (token) => {
+              this.sendTokenToServer(token);
               this.setNotifications(true);
             },
             (error) => {
@@ -42,11 +45,11 @@ export class NotificationsComponent implements OnInit {
   }
 
   requestPermission() {
-    this.afMessaging.requestPermission
-      .pipe(mergeMapTo(this.afMessaging.tokenChanges))
+    this.afMessaging.requestToken
       .subscribe(
         (token) => {
           console.log('Permission granted! Save to the server!', token);
+          this.sendTokenToServer(token);
           this.setNotifications(true);
         },
         (error) => {
@@ -63,17 +66,18 @@ export class NotificationsComponent implements OnInit {
 
   deleteToken() {
     this.afMessaging.getToken
-      .pipe(mergeMap(token => this.afMessaging.deleteToken(token)))
+      .pipe(
+        tap(token => this.deleteTokenFromServer(token)),
+        mergeMap(token => this.afMessaging.deleteToken(token)))
       .subscribe(
-        (token) => {
-          console.log('Deleted!');
+        (success) => {
           this.setNotifications(false);
         },
       );
   }
 
   setNotifications(state: boolean) {
-    if(state) {
+    if (state) {
       this.status = Status.On;
     } else {
       this.status = Status.Off;
@@ -81,6 +85,37 @@ export class NotificationsComponent implements OnInit {
     this.storage.set('notifications', state).subscribe(() => 'Saved Notifications ' + state ? 'enabled' : 'disabled');
   }
 
+  sendTokenToServer(token: string) {
+    console.log('Sending FCM-Token to server: ' + token);
+
+    this.storage.getRef().subscribe(ref => {
+      const fcmTokenCollection: AngularFirestoreCollection<FcmToken> = ref.collection<FcmToken>('fcmToken');
+      fcmTokenCollection.add({token: token}).then(() => console.log('Token saved successfully.'));
+    });
+  }
+
+  deleteTokenFromServer(token: string) {
+    console.log('Deleting FCM-Token from server: ' + token);
+
+    this.storage.getRef().subscribe(ref => {
+      const fcmTokenCollection: AngularFirestoreCollection<FcmToken> = ref.collection<FcmToken>('fcmToken',
+        ref => ref.where('token', '==', token));
+      fcmTokenCollection.get().forEach(querySnapshot => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.delete().then(() => {
+            console.log("Token successfully deleted!");
+          }).catch(function (error) {
+            console.error("Error removing token: ", error);
+          });
+        });
+      })
+    });
+  }
+
+}
+
+export interface FcmToken {
+  token: string
 }
 
 export enum Status {
