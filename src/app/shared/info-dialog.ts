52080c4c53c7cb452de9e17d814e623b278f83e6
mrgoro <mail@mrgoro.de>
2020-01-20 11:31:28 +0100

    Display Version

--- src/app/shared/info-dialog.ts
+++ src/app/shared/info-dialog.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {versions} from "../../environments/versions";
 
 @Component({
   selector: 'info-dialog',
@@ -7,13 +8,18 @@ import { MatDialogRef } from '@angular/material/dialog';
         <h2 mat-dialog-title>Über fi go!</h2>
         Arbeitszeit: 7:48 Stunden<br />
         Pause: 30 Minuten nach Ablauf von 6 Stunden<br />
-        Warnung vor 10-Stunden-Grenze nach 9:30 Stunden<br />
+        Warnung vor 10-Stunden-Grenze nach 9:30 Stunden<br /><br />
+        Version: {{versions.version}}<br />
+        Revision: {{versions.revision}}<br />
+        Branch: {{versions.branch}}<br />
         <div mat-dialog-actions>
             <button mat-button (click)="dialogRef.close(true)">CLOSE</button>
         </div>`,
   styles: []
 })
 export class InfoDialog implements OnInit {
+
+  public versions = versions;
 
   constructor(public dialogRef: MatDialogRef<InfoDialog>) {
   }
