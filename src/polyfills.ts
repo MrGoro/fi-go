041e58d677321aec9d8f2946e30206aacafa9264
Philipp Schürmann <MrGoro@users.noreply.github.com>
2021-08-12 11:12:00 +0000

    ng update @angular/core@12 @angular/cli@12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -71,8 +71,8 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-//import 'zone.js/dist/zone-mix';
+import 'zone.js';  // Included with Angular CLI.
+//import 'zone.js/mix';
 
 
 /***************************************************************************************************
