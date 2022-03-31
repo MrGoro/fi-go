5d2218707e21998fdfaca1ac27658dd27ec54401
mrgoro <mail@mrgoro.de>
2022-03-31 16:23:14 +0200

    Fix GitHub Actions for firebase functions.

--- projects/web/src/app/timer/breaks/breaks-button.component.ts
+++ projects/web/src/app/timer/breaks/breaks-button.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BreaksComponent } from './breaks.component';
+import { AuthService } from '../../auth/util/auth.service';
+import { DataService } from '../util/data.service';
 
 @Component({
   selector: 'app-breaks-button',
   template: `
-    <button mat-icon-button (click)="openBreaks()">
+    <button *ngIf="loggedIn && startTimePresent" mat-icon-button (click)="openBreaks()">
       <mat-icon>lunch_dining</mat-icon>
     </button>
   `
 })
-export class BreaksButtonComponent {
+export class BreaksButtonComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) {}
+  public loggedIn: boolean = false;
+  public startTimePresent: boolean = false;
+
+  constructor(
+    private auth: AuthService,
+    private data: DataService,
+    private dialog: MatDialog
+  ) {}
 
   openBreaks() {
     this.dialog.open(BreaksComponent, {});
+  }
+
+  ngOnInit(): void {
+    this.auth.isLoggedIn().subscribe(loggedIn => {
+      this.loggedIn = loggedIn;
+    });
+    this.data.get('startTime').subscribe(startTime => {
+      this.startTimePresent = startTime !== null;
+    });
   }
 }
