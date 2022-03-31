e51020ce1b15a7edbfead061674bd999ab64f3c2
mrgoro <mail@mrgoro.de>
2022-03-31 16:37:10 +0200

    Feature: Pausen manuell hinzufügen.

--- projects/web/src/app/timer/breaks/breaks-button.component.ts
+++ projects/web/src/app/timer/breaks/breaks-button.component.ts
@@ -3,19 +3,20 @@ import { MatDialog } from '@angular/material/dialog';
 import { BreaksComponent } from './breaks.component';
 import { AuthService } from '../../auth/util/auth.service';
 import { DataService } from '../util/data.service';
+import { map, mergeMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-breaks-button',
   template: `
-    <button *ngIf="loggedIn && startTimePresent" mat-icon-button (click)="openBreaks()">
+    <button *ngIf="display" mat-icon-button (click)="openBreaks()">
       <mat-icon>lunch_dining</mat-icon>
     </button>
   `
 })
 export class BreaksButtonComponent implements OnInit {
 
-  public loggedIn: boolean = false;
-  public startTimePresent: boolean = false;
+  public display: boolean = false;
 
   constructor(
     private auth: AuthService,
@@ -28,11 +29,18 @@ export class BreaksButtonComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.auth.isLoggedIn().subscribe(loggedIn => {
-      this.loggedIn = loggedIn;
-    });
-    this.data.get('startTime').subscribe(startTime => {
-      this.startTimePresent = startTime !== null;
+    this.auth.isLoggedIn().pipe(
+      mergeMap(loggedIn => {
+        if (loggedIn) {
+          return this.data.get('startTime').pipe(
+            map(data => data !== null)
+          );
+        } else {
+          return of(false);
+        }
+      }),
+    ).subscribe(display => {
+      this.display = display;
     });
   }
 }
